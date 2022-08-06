e83948e8d98ca9c562a8fc703146664d1b7775d1
sasakiyudai <yudai14142@gmail.com>
2022-08-06 23:47:22 +0900

    remove user model

--- api/graph/model/models_gen.go
+++ api/graph/model/models_gen.go
@@ -12,9 +12,3 @@ type NewUser struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-
-type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
