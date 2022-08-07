3433631b6b9f6560acdc725b2f57ee57f8829615
sasakiyudai <yudai14142@gmail.com>
2022-08-08 00:44:33 +0900

    use useAppHooks

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,11 @@
 import "../styles/globals.css";
-import {
-  Provider as ReduxProvider,
-  useDispatch,
-  useSelector,
-} from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import type { AppProps } from "next/app";
-import store, { RootState } from "../store";
+import store from "../store";
 import { useEffect } from "react";
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "../lib/apollo-client";
-import { loggined } from "../store/user/action";
+import { useAppHooks } from "../hooks/useAppHooks";
 
 const MyAppWrapper = (props: AppProps) => {
   return (
@@ -23,13 +19,12 @@ const MyAppWrapper = (props: AppProps) => {
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // get user role from redux state
-  const { isLoggedIn } = useSelector((state: RootState) => state.user);
-  const dispatch = useDispatch();
+  const { loginOnLoad, isLoggedIn } = useAppHooks();
 
   useEffect(() => {
     // a redux action that sends a one-time request to "loggined" on initial page load
     // which sets isLoggedIn when resolved
-    if (!isLoggedIn) dispatch(loggined());
+    if (!isLoggedIn) loginOnLoad();
   }, []);
 
   return <Component {...pageProps} />;
